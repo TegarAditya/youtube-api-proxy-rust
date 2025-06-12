29a0439bd5dcf73e1546a4687197c07a82a27135
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-12 15:17:26 +0700

    feat: implement Axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root_handler));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+
+    println!("✅ Server started successfully on http://127.0.0.1:3000");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root_handler() -> &'static str {
+    "Hello, World from Axum!"
 }
