efffbee3f8726892874c57e3d2081d1ea8d4420e
Tegar Aditya <tegaraditya53@gmail.com>
2025-06-18 21:45:04 +0700

    feat: refactor middleware integration and enhance security headers

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,18 @@
 mod handler;
 mod kv_store;
-mod logger;
+mod middleware;
 mod yt_client;
 
 use crate::kv_store::KVStore;
-use crate::logger::log_requests;
 use crate::yt_client::YouTubeClient;
 use axum::{
     Router,
-    http::{HeaderValue, header},
-    middleware::{self},
+    middleware as axum_middleware,
     routing::{delete, get},
 };
 use dotenvy::dotenv;
 use std::env;
 use tower::ServiceBuilder;
-use tower_http::set_header::SetResponseHeaderLayer;
 use tracing::info;
 
 #[derive(Clone)]
@@ -54,23 +51,8 @@ async fn main() {
     info!("✅ KV store and YouTube client initialized successfully");
 
     let middleware_stack = ServiceBuilder::new()
-        .layer(SetResponseHeaderLayer::if_not_present(
-            header::STRICT_TRANSPORT_SECURITY,
-            HeaderValue::from_static("max-age=63072000; includeSubDomains; preload"),
-        ))
-        .layer(SetResponseHeaderLayer::if_not_present(
-            header::CONTENT_SECURITY_POLICY,
-            HeaderValue::from_static("default-src 'self'"),
-        ))
-        .layer(SetResponseHeaderLayer::if_not_present(
-            header::X_CONTENT_TYPE_OPTIONS,
-            HeaderValue::from_static("nosniff"),
-        ))
-        .layer(SetResponseHeaderLayer::if_not_present(
-            header::X_FRAME_OPTIONS,
-            HeaderValue::from_static("DENY"),
-        ))
-        .layer(middleware::from_fn(log_requests));
+        .layer(axum_middleware::from_fn(middleware::helmet::secure_headers))
+        .layer(axum_middleware::from_fn(middleware::logger::log_requests));
 
     let app = Router::new()
         .route("/api/video/{id}", get(handler::find_content))
